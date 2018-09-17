8a9f559f5f33f8e6248dd2bd1a0893f7185f26f3
allexandrucrisan <allexandru.crisan@gmail.com>
2018-09-17 17:24:46 +0300

    Finish UI design. Add links to buttons.

--- app/src/main/java/com/example/minipc/card/MainActivity.java
+++ app/src/main/java/com/example/minipc/card/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.minipc.card;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton ytRedirect;
+    ImageButton fbRedirect;
+    ImageButton wwwRedirect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ytRedirect = (ImageButton) findViewById(R.id.ytButton);
+        ytRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCM0kcPims3a4RFmGtx92M7Q"));
+                startActivity(browserIntent);
+            }
+        });
+
+
+        wwwRedirect = (ImageButton) findViewById(R.id.wwButon);
+        wwwRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bisericabetel.com"));
+                startActivity(browserIntent);
+            }
+        });
+
+        fbRedirect = (ImageButton) findViewById(R.id.fbButton);
+        fbRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/betelmanastur/"));
+                startActivity(browserIntent);
+            }
+        });
+
+
+
+
+
+
     }
 }
